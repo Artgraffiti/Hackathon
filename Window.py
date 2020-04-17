9a6695e6edae25b04962a4ddbcc31f224e1a60c2
Valeriy-Bragin <63748425+Valeriy-Bragin@users.noreply.github.com>
2020-04-17 22:31:12 +0300

    Update Window.py

--- Window.py
+++ Window.py
@@ -3,3 +3,8 @@ class Window:
         self.screen_size = (1200, 800)
         self.bg_color = (255, 255, 255)
         self.human_speed = 70
+
+        self.frame_top_border = 100
+        self.frame_bottom_border = 600
+        self.frame_left_border = 50
+        self.frame_right_border = 550
