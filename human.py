728b4b1da1d197460f35a0bbc2a40f2f98bd771e
Valeriy-Bragin <63748425+Valeriy-Bragin@users.noreply.github.com>
2020-04-17 22:30:00 +0300

    Update human.py

--- human.py
+++ human.py
@@ -8,9 +8,10 @@ class Human:
         self.image = pygame.image.load('images/human.png')
         self.rect = self.image.get_rect()
         self.screen_rect = screen.get_rect()
-
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.centery = self.screen_rect.centery
+        self.settings = settings
+        
+        self.rect.centerx = randint(self.settings.frame_left_border, self.settings.frame_right_border)
+        self.rect.centery = randint(self.settings.frame_top_border, self.settings.frame_bottom_border)      
         self.settings = settings
         self.center_x = float(self.rect.centerx)
         self.center_y = float(self.rect.centery)
@@ -18,11 +19,25 @@ class Human:
 
     def update(self):
         if self.moving:
-            self.center_x += randint(-self.settings.human_speed, self.settings.human_speed) / 10
-            self.center_y += randint(-self.settings.human_speed, self.settings.human_speed) / 10
+            if self.center_y > self.settings.frame_top_border and self.center_y < self.settings.frame_bottom_border:
+                self.center_y += randint(-self.settings.human_speed, self.settings.human_speed) / 10
+            else:
+                if self.center_y <= self.settings.frame_top_border:
+                    self.center_y += randint(10, self.settings.human_speed) / 10
+                if self.center_y >= self.settings.frame_bottom_border:
+                    self.center_y -= randint(10, self.settings.human_speed) / 10
+                
+            if self.center_x < self.settings.frame_right_border and self.center_x > self.settings.frame_left_border:
+                self.center_x += randint(-self.settings.human_speed, self.settings.human_speed) / 10
+            else:
+                if self.center_x >= self.settings.frame_right_border:
+                    self.center_x -= randint(10, self.settings.human_speed) / 10
+                if self.center_x <= self.settings.frame_left_border:
+                    self.center_x += randint(10, self.settings.human_speed) / 10
 
         self.rect.centerx = self.center_x
         self.rect.centery = self.center_y
 
     def blitme(self):
         self.screen.blit(self.image, self.rect)
+
